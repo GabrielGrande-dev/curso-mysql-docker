7f0788b7b4a24478fbd1af000dbec3065002738f
GabrielGrande-dev <gabrielgrande.dev@gmail.com>
2026-03-26 22:38:52 -0300

    Setup inicial: Docker Compose e Script da Aula 1

---
+++ scripts/Aula1.sql
@@ -0,0 +1,15 @@
+/* 
+Comando para acessar o banco via terminal:
+docker exec -it mysql-curso mysql -u root -p
+*/
+
+CREATE TABLE CLIENTE(
+	NOME VARCHAR(30),
+	SEXO CHAR(1),
+	EMAIL VARCHAR(30),
+	CPF INT(11),
+	TELEFONE VARCHAR(30),
+	ENDERECO VARCHAR(100)
+
+);
+
