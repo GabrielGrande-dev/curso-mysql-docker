3866fbe928fbc1df51071c36e2682376bc1cdee6
Gabriel Grande <gabrielgrande.dev@gmail.com>
2026-05-26 13:10:58 -0300

    Add additional statistical queries for salary analysis and ordering

--- POSTGREE/Funções de agregação.sql
+++ POSTGREE/Funções de agregação.sql
@@ -53,3 +53,46 @@ group by departamento;
 select sexo, min(salario), max(salario)
 from funcionarios
 group by sexo;
+
+/*Estatisticas*/
+
+/* Mostrando uma quantidade limitada de linhas */
+select * from funcionarios
+limit 10;
+
+
+/* Qual o gasto total de salario pago pela empresa? */
+select sum(salario) from funcionarios;
+
+
+/* Qual o montante total que cada departamento recebe de salario */
+select departamento, sum(salario)
+from funcionarios
+group by departamento;
+
+
+/* Por departamento, qual o total e a média paga para
+os funcionarios? */
+
+select sum(salario), avg(salario)
+from funcionarios;
+
+select departamento, sum(salario), avg(salario)
+from funcionarios
+group by departamento;
+
+/*ordenando*/
+select departamento, sum(salario), avg(salario)
+from funcionarios
+group by departamento
+order by 3;
+
+select departamento, sum(salario), avg(salario)
+from funcionarios
+group by departamento
+order by 3 ASC;
+
+select departamento, sum(salario), avg(salario)
+from funcionarios
+group by departamento
+order by 3 DESC;
